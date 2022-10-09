158b381bf1791a64920943d2987c19d347b75b47
sukrti <sukrtisharma@gmail.com>
2022-10-09 18:41:56 +0530

    Added classes in API-Console application

---
+++ API-ConsoleApplication/ProductStock/ProductStockHandler.cs
@@ -0,0 +1,36 @@
+﻿using APIBusinessLogic;
+using APIBusinessLogic.Stocks.Contracts;
+using System;
+using System.Threading.Tasks;
+
+namespace API_ConsoleApplication.ProductStock
+{
+    /// <summary>
+    /// This class send the required information to business layer service to update  the stock
+    /// </summary>
+    public class ProductStockHandler
+    {
+        /// <summary>
+        /// send the required information to business layer service to update stock
+        /// </summary>
+        /// <param name="productstockservice"></param>
+        /// <param name="config"></param>
+        /// <param name="productnumber"></param>
+        /// <returns></returns>
+        public static async Task ProductUpdateStockDetails(IProductStockService productstockservice, APIConfigDetails config,string productnumber)
+        {
+            try
+            {
+                
+                await productstockservice.UpdateProductStock(productnumber, 25, config.BaseUrl, config.StockAPI, config.ApiKey);
+
+                System.Console.WriteLine("Stock of product number " + productnumber + " is updated to 25");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                
+            }
+        }
+    }
+}
--- API-ConsoleApplication/Program.cs
+++ API-ConsoleApplication/Program.cs
@@ -1,12 +1,54 @@
-﻿using System;
+﻿using API_ConsoleApplication;
+using APIBusinessLogic.Orders;
+using APIBusinessLogic.Orders.Contracts;
+using APIBusinessLogic.Stocks;
+using APIBusinessLogic.Stocks.Contracts;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading.Tasks;
 
-namespace API_ConsoleApplication
+class program
 {
-    class Program
+    /// <summary>
+    /// Main Entry point of the program . It creates host builder and calls RunAsunc() from ProductHandler 
+    /// </summary>
+    /// <param name="args"></param>
+    public static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        var host = CreateHostBuilder(args).Build();
+        var services = ActivatorUtilities.CreateInstance<ProductHandler>(host.Services);
+        Task task = services.RunAsync();
+        task.Wait(); 
+        Console.ReadLine();
+    }
+    public static IHostBuilder CreateHostBuilder(string[] args)
+    {
+        return Host.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration((context, configuation) =>
+            {
+                configuation.Sources.Clear();
+                configuation.AddJsonFile("Dev.appsettings.json", optional: true, reloadOnChange: true);
+            })
+            .ConfigureServices((context, services) =>
+            {
+                services.AddScoped<IProductOrderService, ProductOrderServices>();
+                services.AddScoped<IProductStockService, ProductStockService>();
+
+                services.AddOptions();
+            });
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
