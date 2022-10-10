f2500beed3fc825e098e50ba66f92cae3bbe2dd6
sukrti <sukrtisharma@gmail.com>
2022-10-10 06:22:44 +0530

    Modified Test project
    Testing done using Bogus and Moq

--- API.Tests/TestOrderService.cs
+++ API.Tests/TestOrderService.cs
@@ -1,6 +1,8 @@
 using APIBusinessLogic.Orders;
 using APIBusinessLogic.Orders.Contracts;
 using APIEntities.OrdersEntity;
+using Bogus;
+using Moq;
 using Newtonsoft.Json;
 using System.IO;
 using System.Linq;
@@ -11,31 +13,33 @@ namespace API.Tests
     public class TestOrderService
     {
         private readonly IProductOrderService _orderService;
-        public TestOrderService()
-        {
-            _orderService = new ProductOrderServices();
-        }
+
         [Fact]
         public void GetTopFiveOrdersRecord_ShouldReturn_CountFive()
         {
+            // Mock productOrderService class to test GetTopFiveRecords() method
+            var productOrderServices = new Mock<ProductOrderServices>();
 
-            var result = _orderService.GetTopFiveRecords(GetRecords());
-            if (result != null)
-                Assert.InRange(result.Count(), 1, 5);
-
+            // Pass if records count is not more than 5
+            Assert.InRange(productOrderServices.Object.GetTopFiveRecords(GetRecords()).Count(), 0, 5);
         }
 
         public Product_CollectionOfReponses GetRecords()
         {
-            var enviroment = System.Environment.CurrentDirectory;
-            var path= Directory.GetParent(enviroment).Parent.Parent.FullName;
-            var actualpath=System.IO.Path.Combine(path, @"MockData.json");
-            using (StreamReader r = new StreamReader(actualpath))
-            {
-                string json = r.ReadToEnd();
-                var rop = JsonConvert.DeserializeObject<Product_CollectionOfReponses>(json);
-                return rop;
-            }
+            var lineFaker = new Faker<Products_LineDetails>()
+                .RuleFor(x => x.MerchantProductNo, faker => faker.Commerce.Product())
+                .RuleFor(x => x.Gtin, faker => faker.Commerce.Ean8())
+                .RuleFor(x => x.Description, faker => faker.Commerce.ProductDescription())
+                .RuleFor(x => x.Quantity, Faker => Faker.Commerce.Random.Number(0, 50));
+
+            var contentFaker = new Faker<Product_Contents>()
+                .RuleFor(x => x.Lines, l => lineFaker.Generate(5).ToList());
+
+            var Product_CollectionOfReponses = new Faker<Product_CollectionOfReponses>()
+               .RuleFor(x => x.Content, c => contentFaker.Generate(100));
+
+            return Product_CollectionOfReponses;
+
         }
     }
 }
