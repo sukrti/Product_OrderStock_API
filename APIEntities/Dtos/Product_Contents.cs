f03cc2528c47ce8af3b3630eb50eae6bea3a737e
sukrti <sukrtisharma@gmail.com>
2022-10-12 12:53:42 +0530

    Code Cleanup

--- APIEntities/Dtos/Product_Contents.cs
+++ APIEntities/Dtos/Product_Contents.cs
@@ -1,6 +1,4 @@
-﻿using APIEntities.Enums;
-using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace APIEntities.OrdersEntity
 {
@@ -9,42 +7,6 @@ namespace APIEntities.OrdersEntity
     /// </summary>
     public class Product_Contents
     {
-        public int Id { get; set; }
-        public string ChannelName { get; set; }
-        public int? ChannelId { get; set; }
-        public string GlobalChannelName { get; set; }
-        public int? GlobalChannelId { get; set; }
-        public Product_ChannelSupport? ChannelOrderSupport { get; set; }
-        public string ChannelOrderNo { get; set; }
-        public Product_Statuses Status { get; set; }
-        public bool IsBusinessOrder { get; set; }
-        public DateTime? CreatedAt { get; set; }
-        public DateTime? UpdatedAt { get; set; }
-        public string MerchantComment { get; set; }
-        public Product_Address BillingAddress { get; set; }
-        public Product_Address ShippingAddress { get; set; }
-        public decimal? SubTotalInclVat { get; set; }
-        public decimal? SubTotalVat { get; set; }
-        public decimal? ShippingCostsVat { get; set; }
-        public decimal TotalInclVat { get; set; }
-        public decimal? TotalVat { get; set; }
-        public decimal? OriginalSubTotalInclVat { get; set; }
-        public decimal? OriginalSubTotalVat { get; set; }
-        public decimal? OriginalShippingCostsInclVat { get; set; }
-        public decimal? OriginalShippingCostsVat { get; set; }
-        public decimal? OriginalTotalInclVat { get; set; }
-        public decimal? OriginalTotalVat { get; set; }
         public List<Products_LineDetails> Lines { get; set; }
-        public string Phone { get; set; }
-        public string Email { get; set; }
-        public string CompanyRegistrationNo { get; set; }
-        public string VatNo { get; set; }
-        public string PaymentMethod { get; set; }
-        public string PaymentReferenceNo { get; set; }
-        public decimal ShippingCostsInclVat { get; set; }
-        public string CurrencyCode { get; set; }
-        public DateTime OrderDate { get; set; }
-        public string ChannelCustomerNo { get; set; }
-        public Dictionary<string, string> ExtraData { get; set; }
     }
 }
--- APIEntities/Dtos/Products_LineDetails.cs
+++ APIEntities/Dtos/Products_LineDetails.cs
@@ -1,40 +1,14 @@
-﻿using APIEntities.Enums;
-using System;
-using System.Collections.Generic;
-
-namespace APIEntities.OrdersEntity
+﻿namespace APIEntities.OrdersEntity
 {
     /// <summary>
     /// This class is a part of CollectionOfMerchantResponses JSON data 
     /// </summary>
     public class Products_LineDetails
     {
-        public Product_Statuses Status { get; set; }
-        public bool IsFulfillmentByMarketplace { get; set; }
         public string Gtin { get; set; }
         public string Description { get; set; }
-        public decimal? UnitVat { get; set; }
-        public decimal? LineTotalInclVat { get; set; }
-        public decimal? LineVat { get; set; }
-        public decimal? OriginalUnitPriceInclVat { get; set; }
-        public decimal? OriginalUnitVat { get; set; }
-        public decimal? OriginalLineTotalInclVat { get; set; }
-        public decimal? OriginalLineVat { get; set; }
-        public decimal OriginalFeeFixed { get; set; }
-        public string JurisCode { get; set; }
-        public string JurisName { get; set; }
-        public decimal VatRate { get; set; }
-        public List<Product_ExtraDates> ExtraData { get; set; }
-        public string ChannelProductNo { get; set; }
         public string MerchantProductNo { get; set; }
         public int Quantity { get; set; }
-        public int CancellationRequestedQuantity { get; set; }
-        public decimal UnitPriceInclVat { get; set; }
-        public decimal FeeFixed { get; set; }
-        public decimal FeeRate { get; set; }
-        public Product_Conditions? Condition { get; set; }
-        public DateTime? ExpectedDeliveryDate { get; set; }
-
     }
 }
 
